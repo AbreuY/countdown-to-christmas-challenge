4008e274448be99ca61221b547ccecdf8411da6c
AbreuY <yolfran12@gmail.com>
2021-12-16 15:00:10 -0500

    changed web icon

--- webpack.config.js
+++ webpack.config.js
@@ -67,7 +67,7 @@ module.exports = {
     //   files: path.resolve(__dirname, "src"),
     // }),
     new HtmlWebpackPlugin({
-        favicon: '4geeks.ico',
+        favicon: 'snowflake.ico',
         template: 'template.html'
     }),
     new PrettierPlugin({
